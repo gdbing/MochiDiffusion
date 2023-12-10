1695e6993f9123eeafeeae13e6b84253e2190c25
Graham Bing <grrraham@gmail.com>
2023-12-09 21:37:49 -0800

    Like models grouped together, submenu with variants

--- Mochi Diffusion/Views/SidebarControls/ModelView.swift
+++ Mochi Diffusion/Views/SidebarControls/ModelView.swift
@@ -10,17 +10,47 @@ import SwiftUI
 
 struct ModelView: View {
     @EnvironmentObject private var controller: ImageController
+    @State private var groupedModels: [String: [SDModel]] = [:]
+
+    func loadModels(_ models: [SDModel]) {
+        let groupedModels = Dictionary(grouping: models) { (model: SDModel) -> String in
+            if model.name.contains("_split-einsum") {
+                return model.name.components(separatedBy: "_split-einsum").first ?? model.name
+            } else {
+                return model.name.components(separatedBy: "_original").first ?? model.name
+            }
+        }
+        self.groupedModels = groupedModels
+    }
 
     var body: some View {
         Text("Model")
             .sidebarLabelFormat()
-        HStack {
-            Picker("", selection: $controller.currentModel) {
-                ForEach(controller.models) { model in
-                    Text(verbatim: model.name).tag(Optional(model))
+        Picker("", selection: $controller.currentModel) {
+            ForEach(Array(groupedModels).sorted { $0.key.localizedCaseInsensitiveCompare($1.key) == .orderedAscending }, id: \.key) { key, models in
+                if models.count > 1 {
+                    Picker(key, selection: $controller.currentModel) {
+                        ForEach(models) { model in
+                            Text(model.name).tag(Optional(model))
+                        }
+                    }.tag({() -> SDModel? in
+                        if let currentModel = controller.currentModel, models.contains(currentModel) {
+                            return Optional(currentModel)
+                        } else {
+                            return models.first
+                        }
+                    }())
+                } else if let model = models.first {
+                    Text(model.name).tag(Optional(model))
                 }
             }
-            .labelsHidden()
+        }
+        .labelsHidden()
+        .onAppear {
+            self.loadModels(controller.models)
+        }
+        .onChange(of: controller.models) { models in
+            self.loadModels(models)
         }
     }
 }
